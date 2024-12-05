7b533e03b2e22fca98685be189fc2e2e4be5dd30
FajriGoBimbel <fajri.gobimbel@gmail.com>
2024-12-05 12:08:48 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,36 @@
-/** @type {import('next').NextConfig} */
+import NextFederationPlugin from "@module-federation/nextjs-mf";
+
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["ui", "zustand"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.unsplash.com",
+      },
+    ],
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "remote",
+          remotes: {},
+          filename: "static/chunks/remoteEntry.js",
+          exposes: {
+            "./remote": "./src/pages",
+          },
+          extraOptions: {
+            exposePages: true,
+          },
+          shared: ["react", "react-dom"],
+        })
+      );
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
